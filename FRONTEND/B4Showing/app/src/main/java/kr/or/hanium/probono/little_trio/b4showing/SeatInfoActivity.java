98c59f862212420792fc268b15911b7b31fc8d0f
jieun <jieun3001@gmail.com>
2018-07-03 01:02:14 +0900

    안드로이드 좌석 확인 액티비티 추가 및 시크바 추가

--- FRONTEND/B4Showing/app/src/main/java/kr/or/hanium/probono/little_trio/b4showing/SeatInfoActivity.java
+++ FRONTEND/B4Showing/app/src/main/java/kr/or/hanium/probono/little_trio/b4showing/SeatInfoActivity.java
@@ -2,12 +2,39 @@ package kr.or.hanium.probono.little_trio.b4showing;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class SeatInfoActivity extends AppCompatActivity {
+
+    @BindView(R.id.seatinfo_seekBar_room_number)
+    SeekBar seekBarRoomNum;
+    @BindView(R.id.seatinfo_textview_number)
+    TextView textViewNumber;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_seat_info);
+        ButterKnife.bind(this);
+        seekBarRoomNum.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                //   tv.setText("onStop TrackingTouch");
+            }
+
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                //  tv.setText("onStart TrackingTouch");
+            }
+
+            public void onProgressChanged(SeekBar seekBar, int progress,
+                                          boolean fromUser) {
+                textViewNumber.setText(String.valueOf(progress));
+            }
+        });
+
+
     }
 }
