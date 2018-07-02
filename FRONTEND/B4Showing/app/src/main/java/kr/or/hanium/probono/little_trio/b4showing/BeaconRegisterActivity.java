7ac44908121fb92d7a88fde83989d981c606956c
jieun <jieun3001@gmail.com>
2018-07-03 00:10:28 +0900

    안드로이드 비콘 입력 화면 레이아웃 구현

---
+++ FRONTEND/B4Showing/app/src/main/java/kr/or/hanium/probono/little_trio/b4showing/BeaconRegisterActivity.java
@@ -0,0 +1,42 @@
+package kr.or.hanium.probono.little_trio.b4showing;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class BeaconRegisterActivity extends AppCompatActivity {
+    @BindView(R.id.beaconregister_button_ok)
+    Button buttonOk;
+    @BindView(R.id.beaconregister_edittext_BeaconNum)
+    EditText editTextBeaconNum;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_beacon_register);
+        ButterKnife.bind(this);
+        clickEvent();
+        buttonOk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(BeaconRegisterActivity.this , StationFindActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+    }
+
+    void clickEvent() {
+
+
+
+    }
+}
---
+++ FRONTEND/B4Showing/app/src/main/java/kr/or/hanium/probono/little_trio/b4showing/SeatInfoActivity.java
@@ -0,0 +1,13 @@
+package kr.or.hanium.probono.little_trio.b4showing;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SeatInfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_seat_info);
+    }
+}
