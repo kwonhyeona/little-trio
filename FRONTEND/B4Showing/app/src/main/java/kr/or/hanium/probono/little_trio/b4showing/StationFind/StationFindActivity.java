c52ff2b486cd1f602e9164a9654c900876213ce2
jieun <jieun3001@gmail.com>
2018-07-08 15:41:01 +0900

    안드로이드 웹뷰 초기 구현

--- FRONTEND/B4Showing/app/src/main/java/kr/or/hanium/probono/little_trio/b4showing/StationFind/StationFindActivity.java
+++ FRONTEND/B4Showing/app/src/main/java/kr/or/hanium/probono/little_trio/b4showing/StationFind/StationFindActivity.java
@@ -3,7 +3,11 @@ package kr.or.hanium.probono.little_trio.b4showing.StationFind;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.KeyEvent;
 import android.view.View;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 import com.melnykov.fab.FloatingActionButton;
 
@@ -16,6 +20,8 @@ public class StationFindActivity extends AppCompatActivity {
 
     @BindView(R.id.stationfind_fab_find)
     FloatingActionButton buttonFind;
+    @BindView(R.id.stationfind_webView_webView)
+    WebView webView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +29,7 @@ public class StationFindActivity extends AppCompatActivity {
         setContentView(R.layout.activity_station_find);
         ButterKnife.bind(this);
         clickEvent();
+        setWebView();
     }
 
     void clickEvent() {
@@ -34,5 +41,51 @@ public class StationFindActivity extends AppCompatActivity {
                 finish();
             }
         });
+    }
+
+    void setWebView() {
+        //배경색 설정
+        webView.setBackgroundColor(0);
+        //스크롤 설정
+        webView.setHorizontalScrollBarEnabled(true);
+        webView.setVerticalScrollBarEnabled(true);
+        //HTML을 파싱하여 웹뷰에서 보여주거나 하는 작업에서
+        //width , height 가 화면 크기와 맞지 않는 현상이 발생한다
+        //이를 잡아주기 위한 코드
+        webView.getSettings().setLayoutAlgorithm(WebSettings.LayoutAlgorithm.SINGLE_COLUMN);
+
+        //캐시파일 사용 금지(운영중엔 주석처리 할 것)
+        //webView.getSettings().setCacheMode(WebSettings.LOAD_NO_CACHE);
+
+        //zoom 허용
+        webView.getSettings().setBuiltInZoomControls(true);
+        webView.getSettings().setSupportZoom(true);
+
+        //javascript의 window.open 허용
+        webView.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
+        //javascript 허용
+        webView.getSettings().setJavaScriptEnabled(true);
+
+        //meta태그의 viewport사용 가능
+        webView.getSettings().setUseWideViewPort(true);
+        webView.loadUrl("http://www.google.com/");
+        webView.setWebViewClient(new WishWebViewClient());
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if ((keyCode == KeyEvent.KEYCODE_BACK) && webView.canGoBack()) {
+            webView.goBack();
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
+    private class WishWebViewClient extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return true;
+        }
     }
 }
