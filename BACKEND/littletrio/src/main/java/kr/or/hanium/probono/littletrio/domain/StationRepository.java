c79e186d7584b28e93c7519f84446887e1ca5006
kwonhyeona <gusdk7656@naver.com>
2018-08-15 14:50:43 +0900

    예전 프로젝트 삭제

--- BACKEND/littletrio/src/main/java/kr/or/hanium/probono/littletrio/domain/StationRepository.java
+++
@@ -1,6 +0,0 @@
-package kr.or.hanium.probono.littletrio.domain;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface StationRepository extends CrudRepository<Station, Long> {
-}
